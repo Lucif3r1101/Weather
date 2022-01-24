50fc77b54473b844c059950ec00d25c4b7e9c029
Lucif3r1101 <rishav1101@gmail.com>
2022-01-24 15:42:16 +0530

    UI DOne

---
+++ app/src/main/java/com/app/weatherwithyou/ScienceFragment.java
@@ -0,0 +1,26 @@
+package com.app.weatherwithyou;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ScienceFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        //Attach a fragment
+        //Add a view
+        //Add an inflator
+
+        View v = inflater.inflate(R.layout.sciencefragment, null);
+        return v;
+    }
+}
